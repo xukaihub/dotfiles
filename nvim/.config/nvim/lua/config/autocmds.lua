33430f2bc3b6c9ea7b00b2edd64318ec1487e7aa
xukai <xukai@nationalchip.com>
2024-06-21 03:39:17 +0000

    Neovim: add auto-save functionality on InsertLeave event

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  callback = function()
+    if vim.bo.modifiable and not vim.bo.readonly then
+      vim.cmd("write")
+    end
+  end,
+})
