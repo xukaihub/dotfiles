dbd0a05293645101f82fd680b7c8d6fea1c04a16
xukai <xukai@nationalchip.com>
2024-11-14 03:11:23 +0000

    feat: add toggleterm.nvim with float mode in LazyVim config
    - Integrated toggleterm.nvim plugin to LazyVim setup for an in-editor terminal
    - Configured terminal to open in float mode for better focus and flexibility
    - Set up float options:
      - Border style: curved
      - Window dimensions: 120x25
      - Window transparency (winblend): 3
    - Mapped <Ctrl-t> to toggle the floating terminal

---
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup({
+        size = 20,
+        open_mapping = [[<c-t>]], -- Key binding to toggle the terminal
+        hide_numbers = true,
+        shade_terminals = true,
+        shading_factor = 2,
+        start_in_insert = true,
+        insert_mappings = true,
+        persist_size = true,
+        direction = "float",
+        close_on_exit = true,
+        shell = vim.o.shell,
+        float_opts = {
+          border = "curved", -- Options: 'single', 'double', 'shadow', 'curved'
+          width = 120,
+          height = 25,
+          winblend = 3,
+        },
+      })
+    end,
+  },
+}
+
